8a81c193ef3e62ded1aa5a2a8c1bd1e8d7360f13
Ashugirl <97950104+Ashugirl@users.noreply.github.com>
2022-01-21 11:03:31 +0100

    Add files via upload

---
+++ triangle/Triangle.java
@@ -0,0 +1,25 @@
+package triangle;
+
+import java.util.Scanner;
+
+public class Triangle {
+	public static void main(String[] arg) {
+	Scanner keyboard = new Scanner(System.in);
+	
+			System.out.println("length side A");
+			int A = keyboard.nextInt();
+			
+			System.out.println("length side B");
+			int B = keyboard.nextInt();
+			
+			System.out.println("length side C");
+			int C = keyboard.nextInt();
+			
+			int result = A+B+C;
+			
+			System.out.println("The perimeter is " + result);
+			keyboard.close();
+		
+	
+	}
+}
