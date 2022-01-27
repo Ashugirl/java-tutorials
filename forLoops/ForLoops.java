d5649cabfd16d742e6ed3fc3b66a8432b4c9b426
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-01-27 10:27:33 +0100

    Add files via upload

---
+++ forLoops/ForLoops.java
@@ -0,0 +1,16 @@
+package forLoops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+	
+		
+		for(int y=-1; y<=29; y=y+3) {
+			System.out.println(y);
+			
+			
+		}
+		
+	}
+}
