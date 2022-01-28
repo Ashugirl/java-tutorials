5b9cab4db952800f8a4e6b2361936b2deea0941f
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-01-28 09:55:37 +0100

    Add files via upload

---
+++ methods/Methods.java
@@ -0,0 +1,29 @@
+package methods;
+
+public class Methods {
+	static void adding(int a, int b) {
+		int sum = a + b;
+		System.out.println("Sum is " + sum);
+	}
+	//static int sum=0;
+	//static void HelloWorld() {
+	//	System.out.println("Hello Method!");	}
+	
+	//static void addNumber(int newNumber) {
+	//	sum += newNumber;
+	//	System.out.println("sum value" + sum);
+	//}
+
+	//static void getSumOfTwoNumber(int n1,  int n2) {
+	//	int getResult = n1+n2;
+	//	System.out.println("Sum result => " + getResult);
+	//}
+	public static void main(String[] args) {
+	//	HelloWorld();
+	//	Message();
+	//	addNumber(10);
+	//	getSumOfTwoNumber(5, 2);
+		adding(1,2);
+		
+	}
+}
