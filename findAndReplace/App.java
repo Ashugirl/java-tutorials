9fdb5fe4e02dab8bc54fda99851b10c463bd62e7
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-01-28 13:14:50 +0100

    Add files via upload

--- findAndReplace/App.java
+++ findAndReplace/App.java
@@ -1,5 +1,20 @@
 package findAndReplace;
 
 public class App {
-
+	static String[] usernames = {"Irina", "Jeremy", "Nazgul"};	
+	
+	static void FindAndReplace(String[] usernames) {
+		for(String x:(usernames)) {
+			if(x.equals("Jeremy")) {
+				System.out.println("EMPTY");
+			
+				
+			}
+		}
+			
+		}
+		
+		public static void main(String[] args) {
+			FindAndReplace("Jeremy");
+		}
 }
